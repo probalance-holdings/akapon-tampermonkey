d3e3bad0a23ca365520a242cc9bcf7b9cd80823c
probalance-holdings <33084729+probalance-holdings@users.noreply.github.com>
2026-02-14 18:37:24 +0900

    Create akapon-project-hide-filter-member-sort_html.user.js

---
+++ scripts/akapon-project-hide-filter-member-sort_html.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         アカポン（プロジェクト｜検索・絞り込み-HTML）
+// @namespace    akapon
+// @version      1.0
+// @match        https://member.createcloud.jp/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-member-sort_html.user.js
+// @downloadURL  https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-member-sort_html.user.js
+// ==/UserScript==
