67f979c779819e3b873fdc0382991f01bb4ff7d5
probalance-holdings <33084729+probalance-holdings@users.noreply.github.com>
2026-02-14 18:31:40 +0900

    Create akapon-hide-Sort-buttons_html.user.js

---
+++ scripts/akapon-hide-Sort-buttons_html.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         アカポン（プロジェクト｜並び替え-HTML）
+// @namespace    akapon
+// @version      1.2
+// @match        https://member.createcloud.jp/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-hide-Sort-buttons_html.user.js
+// @downloadURL  https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-hide-Sort-buttons_html.user.js
+// ==/UserScript==
