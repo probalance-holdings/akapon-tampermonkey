48c90bb07a131c050d6e90901f764aac526f6223
probalance-holdings <33084729+probalance-holdings@users.noreply.github.com>
2026-02-14 18:39:18 +0900

    Create akapon-project-hide-filter-search_css.user.js

---
+++ scripts/akapon-project-hide-filter-search_css.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         アカポン（プロジェクト｜検索・絞り込み）
+// @namespace    akapon
+// @version      1.0
+// @match        https://member.createcloud.jp/*
+// @match        https://akapon.jp/*
+// @match        https://akapon.jp/ai/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-search_css.user.js
+// @downloadURL  https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-search_css.user.js
+// ==/UserScript==
