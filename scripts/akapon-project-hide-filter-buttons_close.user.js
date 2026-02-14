be02180b4b73220ebdf18e19c5378ecf72d4bfd4
probalance-holdings <33084729+probalance-holdings@users.noreply.github.com>
2026-02-14 18:29:53 +0900

    Create akapon-project-hide-filter-buttons_close.user.js

---
+++ scripts/akapon-project-hide-filter-buttons_close.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         アカポン（プロジェクト｜ID～件数ボタンを非表示)
+// @namespace    akapon
+// @version      1.1
+// @match        https://member.createcloud.jp/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-buttons_close.user.js
+// @downloadURL  https://raw.githubusercontent.com/probalance-holdings/akapon-tampermonkey/main/scripts/akapon-project-hide-filter-buttons_close.user.js
+// ==/UserScript==
